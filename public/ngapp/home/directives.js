7444bb36dc4b88935ef6995c18885296e2539fef
Budhram Gurung <budhram.gurung01@gmail.com>
2015-03-27 17:40:14 +0530

    Updated envdependency directive to unregister dependency

--- public/ngapp/home/directives.js
+++ public/ngapp/home/directives.js
@@ -1,70 +1,48 @@
 var directives = angular.module('atlantisApp.homeDirectives', []);
 
-directives.directive("envDepedency", function() {
+directives.directive("envDependency", function() {
   return {
     templateUrl : 'ngapp/templates/dependencies.html',
-    controller: ["$scope", 'appsFactory', function($scope, appsFactory) {
-      appsFactory.currentEnv = $scope.env;
+    controller: ['$scope', '$modal', 'appsFactory', function($scope, $modal, appsFactory) {
+      $scope.hover = function(dep) {
+        return dep.showFlag = !dep.showFlag;
+      };
+
+      $scope.unregisterDependency = function(dep) {
+        var modalInstance = $modal.open({
+          templateUrl: 'ngapp/templates/deleteModal.html',
+          controller: function ($scope, $modalInstance, name) {
+            $scope.confirmName = '';
+            $scope.type = 'dependency name';
+            $scope.itemType = 'dependency';
+            $scope.name = name;
+            $scope.ok = function () {
+              $modalInstance.close(name);
+            };
+
+            $scope.cancel = function () {
+              $modalInstance.dismiss('cancel');
+            };
+          },
+          resolve: {
+            name: function() {
+              return dep.Name;
+            }
+          }
+        });
+
+        modalInstance.result.then(function(name) {
+          $scope.deps = _.filter($scope.deps, function(dep) {
+            return dep.Name !== name;
+          })
+          $scope.addAlert({
+            type: 'success', message: "Dependency '" + name + "' unregistered successfully.",
+            icon: 'glyphicon glyphicon-ok'
+          });
+        }, function(result) {
+          console.log(result);
+        });
+      };
     }],
   };
-});
-
-directives.directive('draggable', function() {
-  return function(scope, element) {
-      var el = element[0];
-
-      el.draggable = true;
-
-      el.addEventListener('dragstart', function(e) {
-        e.dataTransfer.effectAllowed = 'move';
-        e.dataTransfer.clearData('Text');
-        e.dataTransfer.setData('Text', this.id);
-        this.classList.add('drag');
-        return false;
-      }, false);
-
-      el.addEventListener('dragend', function(e) {
-        this.classList.remove('drag');
-        return false;
-      }, false);
-    }
-});
-
-directives.directive('droppable', function() {
-  return {
-    scope: {
-      drop: '&',
-    },
-    link: function(scope, element, attributes) {
-      var el = element[0];
-      el.addEventListener('dragover', function(e) {
-        e.dataTransfer.dropEffect = 'move';
-        if (e.preventDefault) e.preventDefault(); // allows us to drop
-        this.classList.add('over');
-        return false;
-      }, false);
-
-      el.addEventListener('dragenter', function(e) {
-        this.classList.add('over');
-        return false;
-      }, false);
-
-      el.addEventListener('dragleave', function(e) {
-        this.classList.remove('over');
-        return false;
-      }, false);
-
-      el.addEventListener('drop', function(e) {
-        if (e.stopPropagation) e.stopPropagation(); // Stops some browsers from redirecting.
-        this.classList.remove('over');
-
-        var item = document.getElementById(e.dataTransfer.getData('Text'));
-        this.appendChild(item);
-
-        scope.$apply('drop()');
-
-        return false;
-      }, false);
-    }
-  }
 });
