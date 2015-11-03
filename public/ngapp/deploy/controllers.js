480e0987bca700bef4661927bac6a2813b0e0ae8
Budhram Gurung <budhram.gurung01@gmail.com>
2015-11-03 16:21:55 +0530

    Removed deploy tab

--- public/ngapp/deploy/controllers.js
+++
@@ -1,6 +0,0 @@
-var controllers = angular.module('atlantisApp.deployControllers', []);
-
-controllers.controller('DeployCtrl', ['$scope', '$rootScope', '$state',
-  function ($scope, $rootScope, $state) {
-    $rootScope.title = $state.current.title;
-  }]);
